d975f852b04d0391ba33a58440915c855bbf40a1
Serg Klukvin <klukvin@ukr.net>
2020-06-09 20:56:09 +0300

    end

---
+++ js/index.js
@@ -0,0 +1,38 @@
+let template = /\d+/g;
+
+
+// function clickM() {
+  
+//   let tmp = template.exec(qwerty.value);
+
+//   if (tmp !== null) {
+//     console.log(tmp[0])
+//   } else {
+//     return false
+//   }
+
+//   clickM()
+
+// }
+
+
+// function clickM() {
+  
+// console.log(qwerty.value.match(template));
+
+// document.write(qwerty.value.match(template));
+
+// }
+
+
+
+function clickM() {
+  
+console.log(qwerty.value.replace(/[2]/g, 100));
+
+
+
+}
+
+
+
